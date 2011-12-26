078ef9ed1fdeb7b39fc87a44494093f86b25c6a3
karottenreibe <git@rottenrei.be>
2011-12-26 14:21:27 +0100

    drawing nice columns with ugly colors and text

--- ChoreGraph/src/be/rottenrei/android/choregraph/ChoreGraphView.java
+++ ChoreGraph/src/be/rottenrei/android/choregraph/ChoreGraphView.java
@@ -36,10 +36,50 @@ public class ChoreGraphView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
+		int min = 0;
+		int max = 0;
+		for (Chore chore : chores) {
+			min = Math.min(min, chore.getDaysUntilDue());
+			max = Math.max(max, chore.getDaysUntilDue());
+		}
+		int baseline = getHeight() - max;
+		drawBaseline(canvas, baseline);
+
+		float columns = chores.size() * 2 + 1;
+		float columnWidth = getWidth() / columns;
+		float range = (max - min);
+		float oneHeight = getHeight() / range;
+		for (int i = 0; i < chores.size(); i++) {
+			Chore chore = chores.get(i);
+			int column = (i + 1) * 2;
+			float height = oneHeight * chore.getDaysUntilDue();
+			drawColumn(canvas, chore.getName(), column, columnWidth, height, baseline);
+		}
+	}
+
+	private void drawColumn(Canvas canvas, String name, int column, float columnWidth, float height, int baseline) {
 		Paint paint = new Paint();
-		paint.setColor(Color.WHITE);
 		paint.setAntiAlias(true);
-		canvas.drawText("test", 50, 50, paint);
+		paint.setColor(Color.BLUE);
+		float left = ((column - 1) * columnWidth);
+		float top = (height);
+		float right = (column * columnWidth);
+		float bottom = baseline;
+		canvas.drawRect(left, top, right, bottom, paint);
+
+		paint.setColor(Color.RED);
+		float columnMiddle = (left + right) / 2;
+		canvas.save();
+		canvas.rotate(-90, columnMiddle, baseline);
+		canvas.drawText(name, columnMiddle, baseline, paint);
+		canvas.restore();
+	}
+
+	private void drawBaseline(Canvas canvas, int y) {
+		Paint paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setColor(Color.WHITE);
+		canvas.drawLine(0, y, getWidth(), y, paint);
 	}
 
 }
